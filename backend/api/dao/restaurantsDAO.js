e51a0e7422e018f78994a27784ff8ca82424a76b
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-22 21:40:27 +0800

    Added RestaurantsDAO

---
+++ backend/api/dao/restaurantsDAO.js
@@ -0,0 +1,19 @@
+let restaurants;
+
+export default class RestaurantsDAO {
+  static async injectDB(conn) {
+    if (restaurants) {
+      return;
+    }
+
+    try {
+      restaurants = await conn
+        .db(process.env.RESTREVIEWS_NS)
+        .collection("restaurants");
+    } catch (err) {
+      console.error(
+        `Unable to establish a connection handle in RestaurantsDAO: ${e}`
+      );
+    }
+  }
+}
